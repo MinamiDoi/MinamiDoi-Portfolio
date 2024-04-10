bde0df78f1a212e9ba11b2bc7429914bbf48d9d2
MinamiDoi <154774807+MinamiDoi@users.noreply.github.com>
2024-04-10 12:07:40 +0900

    Clinic
    SapporoMiraiClinicフォルダーをコミットします

---
+++ works.clinic/cafeazito/js/function.js
@@ -0,0 +1,30 @@
+$(function() {
+
+// ヘッダー　ナブのサイズ変更
+const windowHeight = $(window).height()
+
+$(window).on('scroll', function(){
+
+const ST = $(window).scrollTop();
+const target = $('.greeting').offset().top;
+
+if (ST > target - 70) {
+ $('.gnav__link').addClass('gnavChange');
+    } else {
+        $('.gnav__link').removeClass('gnavChange');
+        }
+});
+
+
+// スムーススクロール
+$('.click__to').on('click', function(){
+const target = $(this).attr('href');
+const targetPos = $(target).offset().top;
+
+$('html, body').animate({scrollTop: targetPos}, 500);
+
+return false;
+});
+
+
+});
